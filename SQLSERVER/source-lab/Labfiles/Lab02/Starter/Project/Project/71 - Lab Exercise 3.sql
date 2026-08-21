228f850a99978aaf5ae038741c71e4f001c89f35
Sadeed <msadeedashraf@gmail.com>
2026-08-21 19:24:41 -0400

    Add SQL Server course materials

---
+++ SQLSERVER/source-lab/Labfiles/Lab02/Starter/Project/Project/71 - Lab Exercise 3.sql
@@ -0,0 +1,50 @@
+---------------------------------------------------------------------
+-- Module 02
+--
+-- Exercise 3
+---------------------------------------------------------------------
+
+--USE TSQL;
+--GO
+
+---------------------------------------------------------------------
+-- Task 1
+-- 
+-- Observe the results. Why is the result window empty?
+---------------------------------------------------------------------
+
+SELECT	firstname
+		,lastname
+		,city
+		,country
+FROM	HR.Employees
+WHERE	country = 'USA'
+ORDER BY firstname;
+
+
+SELECT	country , count(*)
+FROM	HR.Employees
+group by country
+
+
+SELECT	country , count(*)
+FROM	HR.Employees
+group by country
+having count(*) > 1
+
+
+
+---------------------------------------------------------------------
+-- Task 2
+-- 
+-- Observe that before the USE statement there are the characters 
+-- which means that the USE statement is treated as a comment. There 
+-- is also a block comment around the whole T-SQL SELECT statement. 
+--
+-- Uncomment both statements.
+--
+-- First execute the USE statement and then execute the statement 
+-- starting with the SELECT clause. Observe the results. Notice that 
+-- the results have the same rows as in exercise 1, task 2, but they
+-- are sorted by the lastname column. 
+---------------------------------------------------------------------
